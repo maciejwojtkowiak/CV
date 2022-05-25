ca2f0c762b3bded2d20bc475a01eb1b4b3695a53
Maciej Wojtkowiak <mac.woj1000@gmail.com>
2022-05-25 20:39:17 +0100

    added slider and added left 'bump'

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -5,11 +5,10 @@ const Slider = () => {
     <div className="h-[70vh]">
       <h1 className="text-center text-4xl mt-8">SLIDER</h1>
       <div className="h-full bg-red-100">
-        <motion.div animate={{ scale: 0.5 }}>
+        <motion.div className="h-full" drag="x" dragConstraints={{ right: 0 }}>
           <div className="h-full flex mx-16 mt-4 gap-8">
             <div className="h-5/6 w-1/3 bg-red-500"></div>
             <div className="h-5/6 w-1/3 bg-red-300"></div>
-            <div className="h-5/6 w-1/3 bg-red-400"></div>
             <div className="h-5/6 w-1/3 bg-red-400"></div>
           </div>
         </motion.div>
