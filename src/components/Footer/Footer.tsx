5a025cdea92cf9e99018d36e2278a3a572944e49
Maciej Wojtkowiak <mac.woj1000@gmail.com>
2022-05-26 21:51:54 +0100

    styled footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,12 @@
 const Footer = () => {
   return (
-    <div className="h-20 bg-black border-solid border-b-2 border-indigo-600"></div>
+    <div className="h-20 bg-black border-solid border-t-2 border-indigo-600">
+      <ul className="flex h-full place-content-between items-center text-white px-12">
+        <li className="text-2xl"></li>
+        <li></li>
+        <li className="text-4xl font-[codeFont]">{"</M>"}</li>
+      </ul>
+    </div>
   );
 };
 
