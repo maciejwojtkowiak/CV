06cc315bd4e0aa74353c4f1a097cc0f98a673042
Maciej Wojtkowiak <mac.woj1000@gmail.com>
2022-06-01 22:15:51 +0100

    output technologies which I worked on for a while

--- src/components/Slider/UsedTechnologies/UsedTechnologies.tsx
+++ src/components/Slider/UsedTechnologies/UsedTechnologies.tsx
@@ -1,14 +1,20 @@
 import { SlideList } from "../../../shared/types";
 import Slider from "../Slider";
 import pythonImg from "../../../images/python.png";
-import djangoImg from "../../../images/django.jpg";
+import djangoImg from "../../../images/django.png";
 import dockerImg from "../../../images/docker.png";
+import travisImg from "../../../images/travis.png";
+import firebaseImg from "../../../images/firebase.png";
+import mongoImg from "../../../images/mongo.png";
 
 const UsedTechnologies = () => {
   const technologiesList = [
     { slideName: "Python", slideImgUrl: pythonImg },
     { slideName: "Django", slideImgUrl: djangoImg },
     { slideName: "Docker", slideImgUrl: dockerImg },
+    { slideName: "Travis", slideImgUrl: travisImg },
+    { slideName: "Firebase", slideImgUrl: firebaseImg },
+    { slideName: "MongoDB", slideImgUrl: mongoImg },
   ] as SlideList[];
   return (
     <Slider
