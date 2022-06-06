d2719b5ceebbed4e38ff0d64ea8fe96325f1d75c
Maciej Wojtkowiak <mac.woj1000@gmail.com>
2022-06-06 17:27:06 +0200

    use useId hook

--- src/components/Projects/ProjectItem.tsx
+++ src/components/Projects/ProjectItem.tsx
@@ -1,18 +1,27 @@
+import { useState } from "react";
+import { motion } from "framer-motion";
+
 interface funcProps {
   imgUrl: string;
+  title: string;
 }
 
 const ProjectItem: React.FC<funcProps> = (props) => {
+  const [isHovered, setIsHovered] = useState<Boolean>(false);
   return (
-    <div>
-      <div className="border-4 border-indigo-500">
-        <img
-          src={props.imgUrl}
-          className="object-fill h-full w-full transition hover:blur-sm"
-          alt="recipemaster"
-        />
+    <motion.div
+      className="h-full w-full relative "
+      onHoverStart={() => setIsHovered(true)}
+      onHoverEnd={() => setIsHovered(false)}
+    >
+      <div className=" border-4 border-indigo-500  ">
+        <div className=" h-full w-full">
+          <div className="absolute top-50 right-10">
+            {isHovered && <h2>{props.title}</h2>}
+          </div>
+        </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
--- src/components/Projects/ProjectsGrid.tsx
+++ src/components/Projects/ProjectsGrid.tsx
@@ -1,13 +1,15 @@
 import recipeMaster from "../../images/Projects/recipemaster.png";
 import brabant from "../../images/Projects/brabant.png";
 import ProjectItem from "./ProjectItem";
+import { useId } from "react";
 
 const ProjectsGrid = () => {
+  const id = useId();
   return (
-    <div className="w-full grid place-items-center">
-      <div className="grid grid-cols-2 grid-rows-2 h-screen w-full p-8 gap-8 ">
-        <ProjectItem imgUrl={recipeMaster} />
-        <ProjectItem imgUrl={brabant} />
+    <div className="w-full grid ">
+      <div className="grid  h-screen w-full p-8 gap-8 ">
+        <ProjectItem title="Recipemaster" imgUrl={recipeMaster} />
+        <ProjectItem title="Lion of Brabant" imgUrl={brabant} />
       </div>
     </div>
   );
