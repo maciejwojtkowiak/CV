0587467715b72e5d1fde14c47d15c1e25e0c21e8
Maciej Wojtkowiak <mac.woj1000@gmail.com>
2022-06-06 11:05:02 +0200

    fix footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,10 @@
 const Footer = () => {
   return (
-    <div className="h-20 border-solid border-t-2 border-yellow-700">
-      <ul className="flex h-full place-content-between items-center text-yellow-700 px-12">
+    <div className="h-20 border-solid border-t-2 border-yellow-700 dark:border-white">
+      <ul className="flex h-full place-content-between items-center text-yellow-700 dark:text-white px-12">
         <li className="text-2xl"></li>
         <li></li>
-        <li className="text-4xl font-[codeFont]">{"</M>"}</li>
+        <li className="text-4xl font-[codeFont] ">{"</M>"}</li>
       </ul>
     </div>
   );
