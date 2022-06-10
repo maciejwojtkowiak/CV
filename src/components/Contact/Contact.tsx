adb1816cb7a27f82d9250986515808f653676d42
Maciej Wojtkowiak <mac.woj1000@gmail.com>
2022-06-10 23:54:45 +0200

    fixed slider item

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,12 +1,5 @@
 const Contact = () => {
-  return (
-    <div className="h-[32rem] bg-red-500 grid-cols-2">
-      <form></form>
-      <div>
-        <h4>Informations</h4>
-      </div>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Contact;
