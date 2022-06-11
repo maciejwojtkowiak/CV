1d89748ef4ee85496e7abd63cce7e47ad6363e04
Maciej Wojtkowiak <mac.woj1000@gmail.com>
2022-06-11 19:43:07 +0200

    start creating contact

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,5 +1,22 @@
 const Contact = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1 className="text-yellow-700 text-center text-4xl font-[codeFont]">
+        Contact
+      </h1>
+      <div className="grid grid-cols-2 border-2 border-yellow-700">
+        <div>
+          <form>
+            <input type="email" />
+          </form>
+        </div>
+        <div>
+          <h1>Personal Informations</h1>'
+          <input type="password" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
