6ae9272d3d471f885a903c06a3fc5609d93a4386
Maciej Wojtkowiak <mac.woj1000@gmail.com>
2022-06-11 20:01:48 +0200

    added password to personal data, if it is correct you can check my CV

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,4 +1,14 @@
+import React, { useEffect, useState } from "react";
+const secretPass = "12345";
 const Contact = () => {
+  const [passwordInput, setPasswordInput] = useState<string>("");
+  const [dataIsShown, setDataIsShown] = useState<boolean>(false);
+  const onChangePasswordInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPasswordInput(e.target.value);
+  };
+  useEffect(() => {
+    if (passwordInput === secretPass) setDataIsShown(true);
+  }, [passwordInput]);
   return (
     <div>
       <h1 className="text-yellow-700 text-center text-4xl font-[codeFont]">
@@ -12,7 +22,16 @@ const Contact = () => {
         </div>
         <div>
           <h1>Personal Informations</h1>'
-          <input type="password" />
+          <input
+            value={passwordInput}
+            type="password"
+            onChange={onChangePasswordInput}
+          />
+          {dataIsShown && (
+            <a href="https://drive.google.com/file/d/1AxgMK-n9kZOl3kcQI5yrJxYGWM_1wNDU/view?usp=sharing">
+              My resume
+            </a>
+          )}
         </div>
       </div>
     </div>
